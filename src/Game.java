5c6017ff4cad5642a71337547df6d80b34a66f16
Christopher Bregg <Programming@Skyrim.(none)>
2012-04-22 20:41:47 -0700

    Initial setup. Created Game class stub

---
+++ src/Game.java
@@ -0,0 +1,17 @@
+
+/**
+ * This is the base for a simple guess the number game. It is responsible for running the game loop, getting input, and checking for a correct answer.
+ * @author Christopher Bregg
+ * @version 0.1
+ */
+public class Game {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
